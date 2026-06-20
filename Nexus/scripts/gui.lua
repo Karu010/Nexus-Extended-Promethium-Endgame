4618f0a087b4d7bc57783d591e65a7d7b719f741
sfybb <129160899+sfybb@users.noreply.github.com>
2026-06-20 15:46:39 +0200

    reworked limit gui updates

---
+++ Nexus/scripts/gui.lua
@@ -0,0 +1,149 @@
+-- We sometimes need some logic functions
+local logic = require("__Nexus__.scripts.logic")
+
+local gui = {}
+
+-- GUI name id constants
+local ids = {
+    limit_frame = "nexus_limit_frame",
+    limit_label = "entity_limit",
+    label_cur = 3,
+    label_max = 4,
+}
+
+function gui.get_limit_frame(player, clean_build)
+    local top_gui = player.gui.top
+    local frame = top_gui[ids.limit_frame]
+
+    if clean_build then
+        frame.destroy()
+        frame = nil
+    end
+
+    if frame ~= nil and frame.valid then return frame end
+
+    return top_gui.add{
+        type = "frame",
+        name = ids.limit_frame,
+        direction = "vertical",
+        style = "frame" 
+    }
+end
+
+-- Update the entity limit for a given player and entity name
+-- If the gui exists for this entity, the values for "cur" and "max" are updated
+-- If "cur" or "max" is not defined/nil the value in the gui is not changed
+-- If the gui for this element does not exist, missing values for "cur" and "max" 
+--    are retrieved and a new gui element is created for the entity
+function gui.update_entity_limit(player, entity_name, cur, max)
+    if not player or not player.valid then return end
+    local frame = gui.get_limit_frame(player)
+
+    local entity_entry = frame[entity_name]
+
+    if not entity_entry or not entity_entry.valid or 
+       not entity_entry[ids.limit_label] or not entity_entry[ids.limit_label].valid then
+        -- We have to create gui elements for this entity
+
+        -- Set missing values
+        if cur == nil then cur = player.force.get_entity_count(entity_name) end
+        if max == nil then max = logic.get_machine_limit(player.force, entity_name) end
+
+        gui.create_entry_limit(frame, entity_name, cur, max)
+        return
+    end
+
+    if cur == nil and max == nil then return end
+
+    -- Update existing gui elements
+    local label = entity_entry[ids.limit_label]
+
+    if cur ~= nil then
+        label[ids.label_cur] = cur
+    else
+        cur = label[ids.label_cur]
+    end
+
+    if max ~= nil then
+        label[ids.label_max] = max
+    else
+        max = label[ids.label_max]
+    end
+
+    -- This code part needs to be identical to the equivalent in gui.create_entry_limit (also has this comment)
+    if cur >= max then
+        label.style.font_color = {1, 0.3, 0.3}
+    else
+        label.style.font_color = {1, 1, 1}
+    end
+end
+
+function gui.create_entry_limit(frame, entity_name, cur, max)
+    local entity_entry = frame[entity_name]
+    
+    -- If there exists a valid gui element for this element, remove it first
+    if entity_entry and entity_entry.valid then
+        entity_entry.destroy()
+    end
+
+    entity_entry = frame.add{
+        type = "flow", 
+        direction = "horizontal"
+        name = entity_name
+    }
+
+    entity_entry.add{
+        type = "sprite",
+        sprite = "item/" .. entity_name,
+    }
+
+    entity_entry.add{
+        type = "label",
+        name = ids.limit_label
+        --caption = {"", " ", config.label, ": ", current_count, " / ", max_allowed},
+
+        -- ids.label_cur and ids.label_max need to correspond to the index of cur and max in this localised string
+        caption = {"nexus-mod.gui-machine-limit", {logic.machine_configs[entity_name].label}, cur, max}
+        style = "label"
+    }
+
+    -- This code part needs to be identical to the equivalent in gui.update_entity_limit (also has this comment)
+    if cur >= max then
+        label.style.font_color = {1, 0.3, 0.3}
+    else
+        label.style.font_color = {1, 1, 1}
+    end
+end
+
+function gui.create_all_limits(player, limits, clean_build)
+    if not player or not player.valid then return end
+
+    limits = limits or logic.get_all_machine_limits(player.force)
+
+    local frame = gui.get_limit_frame(player, clean_build)
+    frame.clear() -- Remove children
+
+    for entity_name, limit in pairs(limits) do
+        gui.create_entry_limit(frame, entity_name, limit.cur, limit.max)
+    end
+end
+
+function gui.update_entity_in_force(force, entity_name, cur, max)
+    if not force or not force.valid then return end
+
+    -- Set missing values, in case of missing guis for players
+    if cur == nil then cur = player.force.get_entity_count(entity_name) end
+    if max == nil then max = logic.get_machine_limit(player.force, entity_name) end
+
+    for _, player in pairs(force.players) do
+        gui.update_entity_limit(player, entity_name, cur, max)
+    end
+end
+
+function gui.create_all_in_force(force, clean_build)
+    local limits = logic.get_all_machine_limits(force)
+
+    for _, player in pairs(force.players) do
+        gui.create_all_limits(player, limits, clean_build)
+    end
+end
--- Nexus/scripts/logic.lua
+++ Nexus/scripts/logic.lua
@@ -6,6 +6,7 @@ local logic = {}
 -- Hier kannst du einfach neue Maschinen hinzufügen
 logic.machine_configs = {
     ["zero-point-energy-engine-core"] = {
+        base_tech = "zero-point-energy-engine-core",
         base_limit = 1,
         tech_prefix = "zpe-core-limit-", -- Erwartet zpe-limit-1, zpe-limit-2...
         stages = 3,
@@ -19,18 +20,52 @@ logic.machine_configs = {
     }--]]
 }
 
+function logic.is_limit_upgrade(research_name)
+    for entity_name, conf in pairs(logic.machine_configs) do
+        if research_name == conf.base_tech or 
+           string.find(research_name, conf.tech_prefix, 1, true) ~= nil then
+            return true, entity_name
+        end
+    end
+    return false, nil
+end
+
+function logic.has_machine_limit(entity_name)
+    return logic.machine_configs[entity_name] ~= nil
+end
+
 function logic.get_machine_limit(force, entity_name)
     local config = logic.machine_configs[entity_name]
-    if not config then return 999999 end -- Kein Limit für andere Maschinen
+    if not config then return nil end -- Kein Limit für andere Maschinen
+
+    local base_tech = force.technologies[config.base_tech]
+    if not base_tech or not base_tech.researched then
+        return 0 -- No machines allowed if base tech not researched
+    end
 
     local limit = config.base_limit
     for i = 1, config.stages do
-        local tech_name = config.tech_prefix .. i
-        if force.technologies[tech_name] and force.technologies[tech_name].researched then
+        local tech = force.technologies[config.tech_prefix .. i]
+        if tech and tech.researched then
             limit = limit + 1
+        else
+            -- Either the technology doesn't exist or it's not researched => we dont need to check the stages after
+            break
         end
     end
     return limit
+end
+
+function logic.get_all_machine_limits(force)
+    local limits = {}
+
+    -- Compute limits for all machines with limits
+    for entity_name, _ in pairs(logic.machine_configs) do
+        limits[entity_name].cur = force.get_entity_count(entity_name)
+        limits[entity_name].max = logic.get_machine_limit(force, entity_name)
+    end 
+
+    return limits
 end
 
 return logic
